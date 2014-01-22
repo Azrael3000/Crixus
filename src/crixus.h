07c8a160623c19216d3afe796196d63ed4e7fc25
Arno Mayrhofer <arno.m@gmx.at>
2014-01-22 13:45:41 +0100

    [CleanUp] Removing deprecated function headers for output

--- src/crixus.h
+++ src/crixus.h
@@ -28,14 +28,6 @@ struct OutBuf{
   float x,y,z,nx,ny,nz,vol,surf;
   int kpar, kfluid, kent, kparmob, iref, ep1, ep2, ep3;
 };
-struct gOutBuf{
-  float x, y, z, gam, ggamx, ggamy, ggamz;
-  int id;
-};
-struct linkOutBuf{
-  int id, iggam;
-  float ggam;
-};
 
 //function headers
 int hdf5_output (OutBuf *buf, int len, const char *filename);
